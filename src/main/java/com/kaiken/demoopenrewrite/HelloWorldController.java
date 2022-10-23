dcfc16a7bda7100ef1064e03dfd855c0cd7aff49
Souhayl ABDOUNI <abdouni.souhayl@gmail.com>
2022-10-23 12:15:49 +0200

    Init Repo

--- src/main/java/com/kaiken/demoopenrewrite/HelloWorldController.java
+++ src/main/java/com/kaiken/demoopenrewrite/HelloWorldController.java
@@ -1,11 +1,12 @@
 package com.kaiken.demoopenrewrite;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-  @RequestMapping("/hello")
+  @RequestMapping(value = "/hello", method = RequestMethod.GET)
   public String hello(){
     return "Hello World !";
   }
