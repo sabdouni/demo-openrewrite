35f0c1aa42785051966e33bde3352253a34031eb
Souhayl ABDOUNI <abdouni.souhayl@gmail.com>
2022-10-23 12:05:58 +0200

    Init Repo

---
+++ src/main/java/com/kaiken/demoopenrewrite/DemoOpenrewriteApplication.java
@@ -0,0 +1,13 @@
+package com.kaiken.demoopenrewrite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOpenrewriteApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoOpenrewriteApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/kaiken/demoopenrewrite/DemoOpenrewriteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaiken.demoopenrewrite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoOpenrewriteApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
